83ab15a7ccbf0a17c3767ed1489fd06acb457dc8
Magino <magino@castr.com>
2016-05-29 20:22:23 +0200

    babel + webpack + react + react-dom + hjs-dev-server + app.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,25 @@ const fs      = require('fs');
 const path    = require('path'),
       join    = path.join,
       resolve = path.resolve;
+const root    = resolve(__dirname);
+const src     = join(root, 'src');
+const modules = join(root, 'node_modules');
+const dest    = join(root, 'dist');
 
 const getConfig = require('hjs-webpack');
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+// alternatively, we can use process.argv[1]
+// const isDev = (process.argv[1] || '')
+//                .indexOf('hjs-dev-server') !== -1;
+
+// ...
 
 var config = getConfig({
-  in: join(__dirname, 'src/app.js'),
-  out: join(__dirname, 'dist')
+  isDev: isDev,
+  in: join(src, 'app.js'),
+  out: dest,
+  clearBeforeBuild: true
 });
+
+module.exports = config;
